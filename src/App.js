4edfd1c4be28fd73fca98e89a216f2af88aa9ad5
MIVD <mi.vandeurzen@gmail.com>
2022-08-21 20:35:40 +0200

    team amount change WIP

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import teamFields from './lib/teamFields'
 
 function App() {
   const [teams, setTeams] = useState(teamFields);
+  const [teamAmount, setTeamAmount] = useState(8);
   const [draw, setDraw] = useState(null)
   const [drawCompleted, setDrawCompleted] = useState(false)
-
+  const teamAmountOptions = [4, 8, 16, 32, 64];
   const handleTeamFieldChange = (e, teamIndex) => {
     const copyTeams = [...teams]
     const team = copyTeams.find(team => team.id === teamIndex)
@@ -15,6 +16,26 @@ function App() {
       team.name = e.target.value
       copyTeams[teamIndex] = team
       setTeams([...copyTeams])
+    }
+  }
+
+  const handleTeamAmountChange = (e) => {
+    const newTeamAmount = Number(e.target.value);
+    console.log(newTeamAmount)
+    if(newTeamAmount > teams.length) {
+      const newTeamFields = [];
+      for (let i = 0; i < newTeamAmount; i++) {
+        newTeamFields.push({
+          id: i + teams.length,
+          name: ''
+        })
+      }
+      const teamsToBeAdded = newTeamFields.slice(teams.length)
+      setTeams([...teams, ...teamsToBeAdded])
+    } else {
+      const removeCount = teams.length - newTeamAmount;
+      const sliced = [...teams].slice(removeCount * -1)
+      console.log('sliced', sliced)
     }
   }
 
@@ -30,7 +51,15 @@ function App() {
       <h1>Matchboard</h1>
       {!drawCompleted && 
         <form onSubmit={handleSubmit} className="form-container flex flex-col">
-          <p>To start submit the teams you want to draw</p>
+          <p>How many teams will compete in the tournament?</p>
+          <select defaultValue={teamAmount} name="teamAmount" style={{marginBottom: '8px'}} onChange={handleTeamAmountChange}>
+            {teamAmountOptions.map(option => <option value={option}>{option}</option>)}
+            {/* <option value="volvo">Volvo</option>
+            <option value="saab">Saab</option>
+            <option value="mercedes">Mercedes</option>
+            <option value="audi">Audi</option> */}
+          </select>
+
           {teams.map((team, i) => 
               <div className="team-field" key={team.id}>
                 <input type="text" defaultValue={team.name} onChange={(e) => handleTeamFieldChange(e, i)}/>
@@ -38,7 +67,7 @@ function App() {
             )}
           <button type="submit">Submit</button>
         </form>}
-        
+
       {drawCompleted && 
         <div className="draw-container">
           <h2>{draw.round}</h2>
