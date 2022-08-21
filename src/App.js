724609a332eec6f8f5b810d851ac5196ce4fa37b
MIVD <mi.vandeurzen@gmail.com>
2022-08-21 22:03:34 +0200

    handle team amount change + error handling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import teamFields from './lib/teamFields'
 
 function App() {
   const [teams, setTeams] = useState(teamFields);
-  const [teamAmount, setTeamAmount] = useState(8);
   const [draw, setDraw] = useState(null)
   const [drawCompleted, setDrawCompleted] = useState(false)
+  const [errorMsg, setErrorMsg] = useState('');
   const teamAmountOptions = [4, 8, 16, 32, 64];
+
   const handleTeamFieldChange = (e, teamIndex) => {
     const copyTeams = [...teams]
     const team = copyTeams.find(team => team.id === teamIndex)
@@ -21,7 +22,6 @@ function App() {
 
   const handleTeamAmountChange = (e) => {
     const newTeamAmount = Number(e.target.value);
-    console.log(newTeamAmount)
     if(newTeamAmount > teams.length) {
       const newTeamFields = [];
       for (let i = 0; i < newTeamAmount; i++) {
@@ -34,16 +34,22 @@ function App() {
       setTeams([...teams, ...teamsToBeAdded])
     } else {
       const removeCount = teams.length - newTeamAmount;
-      const sliced = [...teams].slice(removeCount * -1)
-      console.log('sliced', sliced)
+      const sliced = [...teams].slice(0, removeCount * -1)
+      setTeams(sliced)
     }
   }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const draw = new Draw(teams.map(team => team.name))
-    setDraw(draw)
-    setDrawCompleted(true)
+    const competitors = teams.map(team => team.name);
+    if(!competitors.some(competitor => competitor === '')) {
+      const draw = new Draw(competitors)
+      setDraw(draw)
+      setDrawCompleted(true)
+    } else {
+      setErrorMsg('Not enough teams to start draw')
+      setDrawCompleted(false)
+    }
   }
 
   return (
@@ -51,21 +57,25 @@ function App() {
       <h1>Matchboard</h1>
       {!drawCompleted && 
         <form onSubmit={handleSubmit} className="form-container flex flex-col">
-          <p>How many teams will compete in the tournament?</p>
-          <select defaultValue={teamAmount} name="teamAmount" style={{marginBottom: '8px'}} onChange={handleTeamAmountChange}>
-            {teamAmountOptions.map(option => <option value={option}>{option}</option>)}
-            {/* <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option> */}
-          </select>
-
+          <div>
+            <p>How many teams will compete in the tournament?</p>
+            <select 
+              defaultValue={8} 
+              name="teamAmount" 
+              style={{marginBottom: '8px'}} 
+              onChange={handleTeamAmountChange}
+            >
+              {teamAmountOptions.map(option => <option value={option}>{option}</option>)}
+            </select>
+          </div>
+          
           {teams.map((team, i) => 
               <div className="team-field" key={team.id}>
                 <input type="text" defaultValue={team.name} onChange={(e) => handleTeamFieldChange(e, i)}/>
               </div>
             )}
           <button type="submit">Submit</button>
+          {errorMsg !== '' && <p style={{color: 'red'}}>{errorMsg}</p>}
         </form>}
 
       {drawCompleted && 
@@ -75,9 +85,7 @@ function App() {
             <p key={match.matchId}>{match.title}</p>
           )}
           <button onClick={() => setDrawCompleted(false)}>Edit draw</button>
-        </div>
-        }
-      
+        </div>}
     </div>
   );
 }
